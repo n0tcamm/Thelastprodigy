7d7c99db890565f5da62c0111c13692f40c945fd
n0tcamm <111438935+n0tcamm@users.noreply.github.com>
2023-04-25 05:26:06 -0500

    Home

---
+++ src/n0tcamm/Thelastprodigy.java
@@ -0,0 +1,11 @@
+package n0tcamm;
+
+public class Thelastprodigy {
+
+    public Thelastprodigy(){
+        System.out.println("The last prodigy");
+    }
+    public static void main(String[] args) throws Exception {
+        Thelastprodigy thelastprodigy = new Thelastprodigy();
+    }
+}
